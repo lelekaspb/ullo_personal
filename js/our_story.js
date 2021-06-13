0d00d7cc449922b51756b31e458e4eb4e139c309
lelekaspb <bov-1986@yandex.ru>
2021-06-13 18:56:44 +0200

    alignment on the ourstory page

--- js/our_story.js
+++ js/our_story.js
@@ -5,29 +5,28 @@ document
   .querySelector(".sellpoints")
   .style.setProperty("--sellpoint-width", `${sellpointVisualWidth}px`);
 
-const mql = window.matchMedia("(max-width: 450px)");
+const mql = window.matchMedia("(max-width: 700px)");
+
 function countDynamicWidth() {
-  const bigImageWidth = document.querySelector(".image_wrapper").offsetWidth;
-  document
-    .querySelector(".product")
-    .style.setProperty("--dynamic-width", `${bigImageWidth}px`);
   const sellpointVisualWidth =
     document.querySelector(".sellpoint_visual").offsetWidth;
   document
     .querySelector(".sellpoints")
     .style.setProperty("--sellpoint-width", `${sellpointVisualWidth}px`);
   if (mql.matches) {
-    console.log("This is a narrow screen — less than 450px wide.");
-    document.querySelector(".flexibility .sellpoint_text > p").textContent =
-      "With Ullo remote you can take the light with you.";
-    document.querySelector(".colors .sellpoint_text > p").textContent =
-      "With Ullo you get 16 million unique colors per unit of light.";
+    console.log("This is a narrow screen — less than 700px wide.");
+    document.querySelector(".price .sellpoint_text > p").textContent =
+      "Smart lighting should be affordable.";
+    document.querySelector(".friendly .sellpoint_text > p").textContent =
+      "Smart lighting must be for everyone.";
+    document.querySelector(".sustainability .sellpoint_text > p").textContent = "We plant a tree for each bulb sold.";
   } else {
-    console.log("This is a wide screen — more than 450px wide.");
-    document.querySelector(".flexibility .sellpoint_text > p").textContent =
-      "With Ullo Remote you have the ability to control your light without the use of WiFi. You can therefore also take the light with you.";
-    document.querySelector(".colors .sellpoint_text > p").textContent =
-      "With 16 million unique colors per unit of light, you can count on near-infinite color combinations in your home.";
+    console.log("This is a wide screen — more than 700px wide.");
+    document.querySelector(".price .sellpoint_text > p").textContent =
+      "Smart lighting should be affordable. We believe that the spread of smart lighting gives us more joy and energy in everyday life.";
+    document.querySelector(".friendly .sellpoint_text > p").textContent =
+      "Smart lighting must be for everyone. We believe that everyone benefits from better lighting.";
+    document.querySelector(".sustainability .sellpoint_text > p").textContent = "Our products live for decades and rarely need to be replaced. In addition, we plant a tree for each bulb sold.";
   }
 }
 
